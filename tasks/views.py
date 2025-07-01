0807ac3cea929172aedb756dba252dedc292796f
Adrian Kayllan <kayllan.profissional@proton.me>
2025-07-01 07:48:12 -0500

    Adicionando algumas coisas

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+tarefas = ["Ir ao mercado", "Ir a academia", "Estudar para a prova"]
+def index(request):
+    context = {"tarefas": tarefas}
+    return render(request, "tasks/index.html", context)
+
+def adicionar(request):
+    return render(request, "tasks/adicionar.html")
